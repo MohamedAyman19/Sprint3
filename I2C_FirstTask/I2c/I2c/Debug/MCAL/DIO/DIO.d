80b50772e40e07703b82dbd3d7a30e9017e98a3a
MohamedAyman <mohamede.ayman96@gmail.com>
2021-08-07 20:35:21 +0200

    MohamedAyman

---
+++ I2C_FirstTask/I2c/I2c/Debug/MCAL/DIO/DIO.d
@@ -0,0 +1,11 @@
+MCAL/DIO/DIO.d MCAL/DIO/DIO.o: ../MCAL/DIO/DIO.c ../MCAL/DIO/DIO.h \
+ ../MCAL/DIO/DIO_registers.h ../MCAL/DIO/../../typedef.h \
+ ../MCAL/DIO/../../macros.h
+
+../MCAL/DIO/DIO.h:
+
+../MCAL/DIO/DIO_registers.h:
+
+../MCAL/DIO/../../typedef.h:
+
+../MCAL/DIO/../../macros.h:
